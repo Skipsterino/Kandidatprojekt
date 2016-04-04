ad4c957713c8e907dbbed6f58455ac7ad0ab7fac
Joakim Bertils <joabe408@student.liu.se>
2016-04-04 15:36:27 +0200

    Komm init

---
+++ kommunikation/komm_modul/komm_modul/main.c
@@ -0,0 +1,18 @@
+/*
+ * komm_modul.c
+ *
+ * Created: 2016-04-04 15:31:53
+ * Author : Joakim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
