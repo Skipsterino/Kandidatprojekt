9d3500e0ec5e0e8d5280e0f3a21a73bbf10f15a1
Joakim Bertils <joabe408@student.liu.se>
2016-04-04 17:00:24 +0200

    Början på BT i komm

--- kommunikation/komm_modul/komm_modul/main.c
+++ kommunikation/komm_modul/komm_modul/main.c
@@ -6,10 +6,65 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
+
+#define F_CPU 14745600UL
+#define BAUD_RATE 115200
+
+ISR() //Avbrott från buss
+{
+	
+}
+
+ISR() //Avbrott från dator
+{
+	
+}
+
+void initLCD()
+{
+	
+}
+
+void initSPI()
+{
+	
+}
+
+void initBluetooth()
+{
+	uint16_t ubrr_val = 7;
+	
+	//Sätt baud rate prescaler
+	UBRR0L = ubrr_val;
+	UBRR0H = (ubrr_val>>8);
+	
+	//Interrupt-saker 
+	UCSR0B = 0b11011000;
+	
+	//Async
+	//No parity
+	//1 stop bit
+	//8 char size
+	UCSR0C = 0b00000110;
+}
 
 int main(void)
 {
+	//Initiera LCD
+	initLCD();
+	
+	
+	//Initiera buss	
+	initSPI();
+	
+	//Initiera BT
+	initBluetooth();
+	
+	sei();
+	//Sleep
+	
     /* Replace with your application code */
     while (1) 
     {
