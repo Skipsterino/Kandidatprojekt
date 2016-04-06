96d6520b08ab86099f175e50980056e346b2c6db
Joakim Bertils <joabe408@student.liu.se>
2016-04-06 17:14:58 +0200

    Buss start

---
+++ kommunikation/komm_modul/komm_modul/SPI.c
@@ -0,0 +1,41 @@
+/*
+ * SPI.c
+ *
+ * Created: 2016-04-06 16:33:37
+ *  Author: Joakim
+ */ 
+
+#include "SPI.h"
+
+void SPI_init_master()
+{
+	//MOSI och SCK är utgångar
+	DDRB = 0b10100000;	
+	
+	//SPI Enable
+	//SPI Master
+	//SPI clock f/16
+	//SPI interrupt enable
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPIE);
+}
+
+void SPI_init_slave()
+{
+	//MISO är utgång
+	DDRB = 0b01000000;
+	
+	//SPI Enable
+	//SPI interrupt enable
+	SPCR = (1<<SPE)| (1<<SPIE)
+}
+
+void SPI_transmit_master(unsigned char data)
+{
+	SPDR = data;
+}
+
+//SPI Serial transfer complete avbrottsvektor
+ISR(SPI_STC_vect)
+{
+	
+}
---
+++ kommunikation/komm_modul/komm_modul/SPI.h
@@ -0,0 +1,19 @@
+/*
+ * SPI.h
+ *
+ * Created: 2016-04-06 16:32:24
+ *  Author: Joakim
+ */ 
+
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void SPI_init_master();
+void SPI_init_slave();
+
+
+#endif /* SPI_H_ */
