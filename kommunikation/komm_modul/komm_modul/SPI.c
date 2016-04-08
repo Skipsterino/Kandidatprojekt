61e7121103761d77989436c7fc3ea65b72e1b93c
Skipsterino <kevin.kjellen@live.se>
2016-04-08 11:12:17 +0200

    Update SPI.c

--- kommunikation/komm_modul/komm_modul/SPI.c
+++ kommunikation/komm_modul/komm_modul/SPI.c
@@ -10,10 +10,84 @@
 
 uint8_t SPIcounter;
 
+///////////////////////////////////////////////////////////*
+unsigned char fromSen[16];
+unsigned char fromKom[16];
+uint8_t SPIcounter;
+uint8_t toSen;
+uint8_t toKom;
+// timer0 interruppt funktion
+ISR(TIMER0_OVF_vect)
+{
+	SPI_sen_transmit_master();
+}
+
+void SPI_sen_transmit_master()
+{
+	SPIcounter = 0;
+	//SÃ¤tter SS fÃ¶r styr->sen
+	toSen = 1;
+	toKom = 0;
+	Set_SS_sen_kom(toSen, toKom);
+	// skickar noll till Sen
+	SPDR = 0;
+}
+
+ISR(SPI_STC_vect)
+{
+	if(toSen == 1){
+		fromSen[SPIcounter] = SPDR
+		
+		if(SPIcounter < 15){
+			SPDR = 0;
+			++SPIcounter;
+		}
+		else{
+			SPIcounter = 0;
+			//sÃ¤tter SS till styr->kom
+			toSen = 0;
+			toKom = 1;
+			Set_SS_sen_kom(toSen, toKom);
+			//Startar styr->kom buss
+			SPDR = fromSen[SPIcounter];
+		}
+	}
+	else if(toKom == 1){
+		fromKom[SPIcounter] = SPDR
+		
+		if(SPIcounter < 15){
+			SPDR = fromSen[SPIcounter + 1];
+			++SPIcounter;
+		}
+		else{
+			SPIcounter = 0;
+			toSen = 0;
+			toKom = 0;
+			Set_SS_sen_kom(toSen, toKom);
+		}
+	}
+}
+
+void Set_SS_sen_kom(uint8_t toSen, uint8_t toKom)
+{
+	if(toSen == 1){
+		PORTB &= ~(1 << 3);
+		PORTB |= (1 << 4);
+	}
+	else if(toKom == 1){
+		PORTB |= (1 << 3);
+		PORTB &= ~(1 << 4);
+	}
+	else{
+		PORTB |= (1 << 3);
+		PORTB |= (1 << 4);
+	}
+}
+
 void SPI_init_master()
 {
-	//MOSI och SCK är utgångar
-	DDRB |= 0b10100000;	
+	//MOSI, SCK , SS0 och SS1 Ã¤r utgÃ¥ngar
+	DDRB |= 0b10111000;	
 	
 	//SPI Enable
 	//SPI Master
@@ -21,12 +95,28 @@ void SPI_init_master()
 	//SPI interrupt enable
 	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPIE);
 	
+	// Enable timer0 interruppt
+	TIMSK = (1<<TOIE0);
+	// set timer0 counter initial value to 0
+	TCNT0=0x00;
+	// start timer0 with /1024 prescaler
+	TCCR0 = (1<<CS02) | (1<<CS00);
+	
 	SPIcounter = 0;
+	uint8_t toSen = 0;
+	uint8_t toKom = 0;
+	
+	Set_SS_sen_kom(toSen, toKom);
+	
+	// Activale global interruppt
+	sei();
 }
+
+*//////////////////////////////////////////////////////////
 
 void SPI_init_slave()
 {
-	//MISO är utgång
+	//MISO Ã¤r utgÃ¥ng
 	DDRB |= 0b01000000;
 	
 	//SPI Enable
@@ -34,12 +124,6 @@ void SPI_init_slave()
 	SPCR = (1<<SPE)| (1<<SPIE);
 	
 	SPIcounter = 0;
-}
-
-void SPI_transmit_master(unsigned char data)
-{
-	SPIcounter = 0;
-	SPDR = data;
 }
 
 //SPI Serial transfer complete avbrottsvektor
