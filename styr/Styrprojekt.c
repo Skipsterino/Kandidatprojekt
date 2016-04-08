52e073d0fd80ce5dcf29301f06550db7e27f2554
erilju719 <erik.ljungzell@live.se>
2016-04-08 14:10:07 +0200

    swag

--- styr/Styrprojekt.c
+++ styr/Styrprojekt.c
@@ -13,6 +13,11 @@
 #include <math.h>
 #include <util/delay.h>
 
+#define SS_sen 3
+#define SS_kom 4
+
+
+//structs som används i koden
 typedef struct{
 	float a;
 	float b;
@@ -23,13 +28,17 @@ typedef struct{
 	unsigned char a;
 	unsigned char b;
 	unsigned char c;
-} triple_uchar;
+	unsigned char d;
+	unsigned char e;
+	unsigned char f;
+} six_uchar;
 
 typedef struct{
 	unsigned char a;
 	unsigned char b;
 } double_uchar;
 
+//Deklarationer
 void Init(void);
 void USART_Init(unsigned long);
 void USART_Transmit(unsigned char);
@@ -40,18 +49,19 @@ void Send_Servo_Message(unsigned char message[], uint8_t num_of_par);
 void Send_Servo_Delaytime(void);
 void Send_Servo_Position(unsigned char ID_and_pos[]);
 void Send_Servo_LED(void);
-triple_uchar Kar_To_Pos(float kar[]);
-triple_float Pos_To_Kar(unsigned char pos[]);
-triple_float create_triple_float(float a, float b, float c);
-triple_uchar create_triple_uchar(unsigned char a, unsigned char b, unsigned char c);
-double_uchar create_double_float(unsigned char a, unsigned char b);
 void Send_Servo_Velocity(unsigned char ID_and_vel[]);
-double_uchar Get_Servo_Position(unsigned char ID); 
+double_uchar Get_Servo_Position(unsigned char ID);
 
+six_uchar Kar_To_Pos(triple_float kar);
+triple_float Pos_To_Kar(six_uchar pos);
 
-	
+triple_float create_triple_float(float a, float b, float c);
+six_uchar create_six_uchar(unsigned char a, unsigned char b, unsigned char c, unsigned char d, unsigned char e, unsigned char f);
+double_uchar create_double_uchar(unsigned char a, unsigned char b);
 
-//unsigned char message[]* Receive_Servo_Statuspackage(uint8_t size);
+void SPI_init_master();
+void SPI_sen_transmit_master();
+void Set_SS_sen_kom(uint8_t toSen, uint8_t toKom);
 
 //Dessa ska INTE vara globala sen (om de används senare)! Är globala endast vid testning så komp. inte slänger dem pga oanvända
 //unsigned char r1; //I dessa lagras statuspaketets bytes
@@ -63,26 +73,47 @@ double_uchar Get_Servo_Position(unsigned char ID);
 	
 int main(void)
 {
-	//INIT
-	DDRD = 1<<DDD2;
-	DDRC = 0; //JTAG, alla väljs till ingångar
-	DDRB = (1<<DDB3) | (1<<DDB4) | (1<<DDB5) | (0<<DDB6) | (1<<DDB7); //SPI, allt ut förutom PB6
+	Init();
 	USART_Init(BAUD_PRESCALER);
-	sei(); //Aktivera avbrott öht (MSB=1 i SREG)
+	SPI_init_master();
 	//KÖR FÖLJANDE FUNKTIONER NÄR SERVONA BEHÖVER KALIBRERAS PÅ NÅGOT SÄTT
 	//Send_Servo_Delaytime();
-	//Send_Servo_LED();
+	Send_Servo_LED();
+	//Send_Servo_Angle_Limit();
+	////Send_Servo_Position({0x07,0x66,0x01});
+	//_delay_ms(1000);
+	////Send_Servo_Position({0x07,0xFF,0x01});
+	//_delay_ms(1000);
+	////Send_Servo_Position({0x07,0x66,0x01});
+	//_delay_ms(1000);
+	////Send_Servo_Position({0x07,0xFF,0x01});
+	//_delay_ms(1000);
+	//Send_Servo_Position({0x07,0x65,0x01});
+
+	sei(); //Aktivera avbrott öht (MSB=1 i SREG)
 	
-	unsigned char servos[9] = {12,10,8,18,16,14, 6,4,2};
-	for (uint8_t i = 0; i < 9; i++)
-	{		
-		unsigned char ID_and_position[3] = {servos[i], 0x90, 0x01};
-		Send_Servo_Position(ID_and_position);
-		_delay_ms(1000);
-	}
+
+	//unsigned char servos[9] = {12,10,8,18,16,14, 6,4,2};
+	//for (uint8_t i = 0; i < 9; i++)
+	//{		
+		//unsigned char ID_and_position[3] = {servos[i], 0x90, 0x01};
+		//Send_Servo_Position(ID_and_position);
+		//_delay_ms(1000);
+	//}
+	
 	while(1)
 	{
     }
+}
+
+//Konfigurerar portar, initierar UART och aktiverar avbrott öht
+void Init(void)
+{
+	DDRD = 1<<DDD2;
+	DDRC = 0; //JTAG, alla väljs till ingångar
+	DDRB = (1<<DDB3) | (1<<DDB4) | (1<<DDB5) | (0<<DDB6) | (1<<DDB7); //SPI, allt ut förutom PB6
+	
+	USART_Init(BAUD_PRESCALER);
 }
 
 void USART_Init( unsigned long prescaler )
@@ -114,8 +145,7 @@ unsigned char USART_Receive( void )
 	return UDR0;
 }
 
-
-// id, length, instruktion,startposition, och parametrar i en array och ta med antalet parametrar som en uint8 
+// id,instruktion,startposition, och parametrar i en array och ta med antalet parametrar som en uint8 
  void Send_Servo_Message(unsigned char message[], uint8_t num_of_par)
 {
 	PORTD |= 1<<PORTD2; //Välj riktning "till servon" i tri-state
@@ -226,119 +256,6 @@ void Send_Servo_LED(void)
 	}
 }
 
-//Räknar ut kartesiska koordinater (kar) mha servoposition (pos)
-triple_float Pos_To_Kar(unsigned char pos[])
-{
-	float pos_to_ang_scale = 0.0051182676011; //Skalfaktor mellan pos och vinkel i RADIANER
-	unsigned char first_servo_offset = 0x01; //Lika med 0?
-	unsigned char second_servo_offset = 0x01; //Lika med 0?
-	unsigned char third_servo_offset = 0x01; //Skild från 0
-	float leg_offset_x = 15;
-	float leg_offset_y = 10;
-	float leg_offset_z = 0; 
-	
-	float L1 = 5; //Benlängder i cm
-	float L2 = 7; 
-	float L3 = 12;
-	 
-	unsigned char p1 = pos[1];
-	unsigned char p2 = pos[2];
-	unsigned char p3 = pos[3];
-		
-	float v1= pos_to_ang_scale * (float)(p1 - 0x1FF + first_servo_offset);
-	float v2= pos_to_ang_scale * (float)(p2 - 0x1FF + second_servo_offset);
-	float v3= pos_to_ang_scale * (float)(p3 - 0x1FF + third_servo_offset);
-	 
-	float x = L1*sin(v1) + L2*sin(v1)*cos(v2) + L3*cos(v1)*cos(v2 + v3) + leg_offset_x;
-	float y = L1*cos(v1) + L2*cos(v1)*cos(v2) + L3*cos(v1)*cos(v2 + v3) + leg_offset_y;
-	float z = -L2*sin(v2) - L3*sin(v2 + v3) + leg_offset_z;
-	triple_float kar = create_triple_float(x,y,z); 
-	return kar;
-}
-
-triple_uchar Kar_To_Pos(float kar[])
-{
-	float ang_to_pos_scale = 195.37860814;
-	float pi = 3.1415926536;
-	
-	unsigned char first_servo_offset = 0x01; //Lika med 0?
-	unsigned char second_servo_offset = 0x01; //Lika med 0?
-	unsigned char third_servo_offset = 0x01; //Skild från 0
-	
-	float L1 = 5; //Benlängder i cm
-	float L2 = 7;
-	float L2_power_of_2 = 49;
-	float L3 = 12;
-	float L3_power_of_2 = 144;
-	
-	float leg_offset_x = 15;
-	float leg_offset_y = 10;
-
-	
-	float x = kar[0];
-	float y = kar[1];
-	float z = kar[2];
-	
-	//hjälpvariabler
-	float r_leg = sqrt(pow((x-leg_offset_x),2)+pow((y-leg_offset_y),2));
-	float r_outer_leg = r_leg - L1;
-	float r_outer_leg_power_of_2 = pow(r_outer_leg,2); 
-	float z_power_of_2= pow(z,2);
-	float big_ass_thing = asin((L3_power_of_2-r_outer_leg_power_of_2-z_power_of_2-L2_power_of_2)/(-2*L2)/sqrt(r_outer_leg_power_of_2+z_power_of_2));
-
-	//inverskinematik
-	float v1 = asin((y-leg_offset_y)/r_leg);
-	float v2 = big_ass_thing + atan(r_outer_leg/z);
-	float v3 = pi -big_ass_thing + asin((L2_power_of_2-r_outer_leg_power_of_2-z_power_of_2-L3_power_of_2)/(-2*L3)/sqrt(r_outer_leg_power_of_2+z_power_of_2));
-
-	 
-	unsigned char pos1 =(unsigned char)(ang_to_pos_scale*v1) + 0x1FF + first_servo_offset;
-	unsigned char pos2 =(unsigned char)(ang_to_pos_scale*v2) + 0x1FF + second_servo_offset;
-	unsigned char pos3 =(unsigned char)(ang_to_pos_scale*v3) + 0x1FF + third_servo_offset; 
-	triple_uchar pos = create_triple_uchar(pos1, pos2, pos3);
-	return pos; 
-	
-}
-
-triple_float create_triple_float(float a, float b, float c)
-{
-	triple_float this; 
-	this.a = a;
-	this.b = b;
-	this.c = c;
-	return this; 
-}
-
-triple_uchar create_triple_uchar(unsigned char a, unsigned char b, unsigned char c)
-{
-	triple_uchar this;
-	this.a = a;
-	this.b = b;
-	this.c = c;
-	return this;
-}
-
-
-double_uchar create_double_uchar(unsigned char a, unsigned char b)
-{
-	double_uchar this;
-	this.a = a;
-	this.b = b;
-	return this;
-}
-
-//Konfigurerar portar, initierar UART och aktiverar avbrott öht
-void Init(void)
-{
-	DDRD = 1<<DDD2;
-	DDRC = 0; //JTAG, alla väljs till ingångar
-	DDRB = (1<<DDB3) | (1<<DDB4) | (1<<DDB5) | (0<<DDB6) | (1<<DDB7); //SPI, allt ut förutom PB6
-	
-	USART_Init(BAUD_PRESCALER);
-	
-	sei(); //Aktivera avbrott öht (MSB=1 i SREG)
-}
-
 //Skickar önskad hastighet till servo. Inargument = (ID, hastighet) OBS! LS Byte först...
 void Send_Servo_Velocity(unsigned char ID_and_vel[])
 {
@@ -376,53 +293,304 @@ double_uchar Get_Servo_Position(unsigned char ID) //FUNKAR ATT RETURNERA SÅHÄR
 	position_LSByte = USART_Receive(); //LS Byte av positionen
 	position_MSByte = USART_Receive(); //MS Byte av positionen
 	USART_Receive(); //Checksum
-	
 
 	double_uchar position = create_double_uchar(position_LSByte, position_MSByte);
 	return position;
 }
 
-//Konfigurerar alla servon med vinkelbegränsningar
-void Send_Servo_Angle_Limit(void)
+//Räknar ut kartesiska koordinater (kar) mha servoposition (pos). Returnerar kar som en triple_float.
+triple_float Pos_To_Kar(six_uchar pos)
 {
-	uint8_t inners_middle[] = {13,14};
-	uint8_t inners_lf_rb[] = {1,8};
-	uint8_t inners_rf_lb[] = {2,7};
-	uint8_t middle[] = {3,4,9,10,15,16};
-	uint8_t outer_left[] = {6,12,18};
-	uint8_t outer_right[] = {5,11,17};
-
+	float pos_to_ang_scale = 0.0051182676011; //Skalfaktor mellan pos och vinkel (som mäts i RADIANER)
+	unsigned int first_servo_offset = 0x01; //Lika med 0?
+	unsigned int second_servo_offset = 0x01; //Lika med 0?
+	unsigned int third_servo_offset = 0x01; //Skild från 0
+	float leg_offset_x = 15;
+	float leg_offset_y = 10;
+	float leg_offset_z = 0; 
 	
-	for (uint8_t i = 0; i < 6; i++)
-	{
-		unsigned char return_delay_time[] = {middle[i], 0x07, 0x03, 0x06, 0xCD, 0x00, 0X31, 0x03}; // pos 1FF -+ 1024/300pos/vinkl*90grader= 0x00CD,0x0331
-		Send_Servo_Message(return_delay_time, 5);
-		_delay_ms(500);
-	}
-	for (uint8_t i = 0; i < 3; i++) 
-	{
-		unsigned char return_delay_time[] = {outer_left[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-50grader eller + 90 grader)= 0x0155,0x0331
-		Send_Servo_Message(return_delay_time, 2);
-		_delay_ms(500);
-		unsigned char return_delay_time[] = {outer_right[i], 0x07, 0x03, 0x06, 0xCD, 0x00, 0xA9, 0x02}; // pos 1FF + (1024/300pos/vinkl*(+50grader eller - 90 grader)= 0x00CD,0x02A9
-		Send_Servo_Message(return_delay_time, 2);
-		_delay_ms(500);
-	}
-	for (uint8_t i = 0; i < 2; i++) 
-	{
-		unsigned char return_delay_time[] = {inner_lf_rb[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-60grader eller + 45 grader)= 0x01EE,0x0298
-		Send_Servo_Message(return_delay_time, 2);
-		_delay_ms(500);
-		unsigned char return_delay_time[] = {inner_middle[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-15grader eller + 15 grader)= 0x01CC,0x0232
-		Send_Servo_Message(return_delay_time, 2);
-		_delay_ms(500);
-		unsigned char return_delay_time[] = {inner_rf_lb[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-45grader eller + 60 grader)= 0x0166,0x02CB
-		Send_Servo_Message(return_delay_time, 2);
-		_delay_ms(500);
-	}
+	float L1 = 5; //Benlängder i cm
+	float L2 = 7; 
+	float L3 = 12;
 	
+	//Plocka fram LS resp. MS Byte av servopositionerna
+	unsigned char p1LS = pos.a;
+	unsigned char p1MS = pos.b;
+	unsigned char p2LS = pos.c;
+	unsigned char p2MS = pos.d;
+	unsigned char p3LS = pos.e;
+	unsigned char p3MS = pos.f;
+	
+	//Kombinera dessa till unsigned ints ty passar bättre för nedanstående beräkningar
+	unsigned int p1 = p1MS; //OBS, här hamnar MS på LS-platserna i p1, varför nedanstående skiftning görs så MS hamnar rätt i p1
+	p1 = (p1<<8);
+	p1 |= (unsigned int)p1LS; //Adderar till LS på LS-platserna i p1
+	unsigned int p2 = p2MS; 
+	p2 = (p2<<8);
+	p2 |= (unsigned int)p2LS;
+	unsigned int p3 = p3MS;
+	p3 = (p3<<8);
+	p3 |= (unsigned int)p3LS;
+		
+	float v1= pos_to_ang_scale * (float)(p1 - 0x1FF + first_servo_offset);
+	float v2= pos_to_ang_scale * (float)(p2 - 0x1FF + second_servo_offset);
+	float v3= pos_to_ang_scale * (float)(p3 - 0x1FF + third_servo_offset);
+	 
+	float x = L1*sin(v1) + L2*sin(v1)*cos(v2) + L3*cos(v1)*cos(v2 + v3) + leg_offset_x;
+	float y = L1*cos(v1) + L2*cos(v1)*cos(v2) + L3*cos(v1)*cos(v2 + v3) + leg_offset_y;
+	float z = -L2*sin(v2) - L3*sin(v2 + v3) + leg_offset_z;
+	triple_float kar = create_triple_float(x,y,z); 
+	return kar;
 }
 
+//Räknar ut servopositioner (pos) mha kartesiska koordinater (kar). Returnerar positionerna som en six_uchar. LS Byte först för varje servoposition! (pos1LS, pos1MS,...)
+six_uchar Kar_To_Pos(triple_float kar)
+{
+	float ang_to_pos_scale = 195.37860814;
+	float pi = 3.1415926536;
+	
+	unsigned int first_servo_offset = 0x01; //Lika med 0?
+	unsigned int second_servo_offset = 0x01; //Lika med 0?
+	unsigned int third_servo_offset = 0x01; //Skild från 0
+	
+	float L1 = 5; //Benlängder i cm
+	float L2 = 7;
+	float L2_sq = 49;
+	float L3 = 12;
+	float L3_sq = 144;
+	
+	float leg_offset_x = 15;
+	float leg_offset_y = 10;
+	
+	float x = kar.a;
+	float y = kar.b;
+	float z = kar.c;
+	
+	//Hjälpvariabler
+	float r_leg = sqrt(pow((x-leg_offset_x),2)+pow((y-leg_offset_y),2));
+	float r_outer_leg = r_leg - L1;
+	float r_outer_leg_sq = pow(r_outer_leg,2); 
+	float z_sq= pow(z,2);
+	float big_ass_thing = asin((L3_sq-r_outer_leg_sq-z_sq-L2_sq)/(-2*L2)/sqrt(r_outer_leg_sq+z_sq));
 
+	//Invers kinematik
+	float v1 = asin((y-leg_offset_y)/r_leg);
+	float v2 = big_ass_thing + atan(r_outer_leg/z);
+	float v3 = pi -big_ass_thing + asin((L2_sq-r_outer_leg_sq-z_sq-L3_sq)/(-2*L3)/sqrt(r_outer_leg_sq+z_sq));
+
+	//Spara först positionerna som unsigned int (beräkningarna passar bättre med att hantera både LS och MS byte samtidigt som en unsigned int) 
+	unsigned int pos1 =(unsigned int)(ang_to_pos_scale*v1) + 0x1FF + first_servo_offset;
+	unsigned int pos2 =(unsigned int)(ang_to_pos_scale*v2) + 0x1FF + second_servo_offset;
+	unsigned int pos3 =(unsigned int)(ang_to_pos_scale*v3) + 0x1FF + third_servo_offset;
+	
+	//Dela upp positionen i LS respektive MS Byte, spara i unsigned chars (unsigned chars passar bättre vid kontakt med servona)
+	unsigned char pos1LS = pos1; //Plockar ut LS Byte av positionen för det första servot (översta servot) 
+	unsigned char pos1MS = (pos1>>8); //Som ovan men med MS Byte
+	unsigned char pos2LS = pos2;
+	unsigned char pos2MS = (pos2>>8);
+	unsigned char pos3LS = pos3;
+	unsigned char pos3MS = (pos3>>8);
+	
+	six_uchar pos = create_six_uchar(pos1LS, pos1MS, pos2LS, pos2MS, pos3LS, pos3MS);
+	return pos; 
+}
+
+//Returnerar en triple_float bestående av tre givna floats.
+triple_float create_triple_float(float a, float b, float c)
+{
+	triple_float this; 
+	this.a = a;
+	this.b = b;
+	this.c = c;
+	return this; 
+}
+
+//Returnerar en six_uchar bestående av sex givna unsigned chars.
+six_uchar create_six_uchar(unsigned char a, unsigned char b, unsigned char c, unsigned char d, unsigned char e, unsigned char f)
+{
+	six_uchar this;
+	this.a = a;
+	this.b = b;
+	this.c = c;
+	this.d = d;
+	this.e = e;
+	this.f = f;
+	return this;
+}
+
+//Returnerar en double_uchar bestående av två givna unsigned chars.
+double_uchar create_double_uchar(unsigned char a, unsigned char b)
+{
+	double_uchar this;
+	this.a = a;
+	this.b = b;
+	return this;
+}
+
+//Konfigurerar alla servon med vinkelbegränsningar
+//void Send_Servo_Angle_Limit(void)
+//{
+	//uint8_t inners_middle[] = {13,14};
+	//uint8_t inners_lf_rb[] = {1,8};
+	//uint8_t inners_rf_lb[] = {2,7};
+	//uint8_t middle[] = {3,4,9,10,15,16};
+	//uint8_t outer_left[] = {6,12,18};
+	//uint8_t outer_right[] = {5,11,17};
+//
+	//
+	//for (uint8_t i = 0; i < 6; i++)
+	//{
+		//unsigned char return_delay_time[] = {middle[i], 0x07, 0x03, 0x06, 0xCD, 0x00, 0X31, 0x03}; // pos 1FF -+ 1024/300pos/vinkl*90grader= 0x00CD,0x0331
+		//Send_Servo_Message(return_delay_time, 5);
+		//_delay_ms(500);
+	//}
+	//for (uint8_t i = 0; i < 3; i++) 
+	//{
+		//unsigned char return_delay_time[] = {outer_left[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-50grader eller + 90 grader)= 0x0155,0x0331
+		//Send_Servo_Message(return_delay_time, 2);
+		//_delay_ms(500);
+		//unsigned char return_delay_time[] = {outer_right[i], 0x07, 0x03, 0x06, 0xCD, 0x00, 0xA9, 0x02}; // pos 1FF + (1024/300pos/vinkl*(+50grader eller - 90 grader)= 0x00CD,0x02A9
+		//Send_Servo_Message(return_delay_time, 2);
+		//_delay_ms(500);
+	//}
+	//for (uint8_t i = 0; i < 2; i++) 
+	//{
+		//unsigned char return_delay_time[] = {inner_lf_rb[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-60grader eller + 45 grader)= 0x01EE,0x0298
+		//Send_Servo_Message(return_delay_time, 2);
+		//_delay_ms(500);
+		//unsigned char return_delay_time[] = {inner_middle[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-15grader eller + 15 grader)= 0x01CC,0x0232
+		//Send_Servo_Message(return_delay_time, 2);
+		//_delay_ms(500);
+		//unsigned char return_delay_time[] = {inner_rf_lb[i], 0x07, 0x03, 0x06, 0x55 0x01, 0x31 0x03}; // pos 1FF + (1024/300pos/vinkl*(-45grader eller + 60 grader)= 0x0166,0x02CB
+		//Send_Servo_Message(return_delay_time, 2);
+		//_delay_ms(500);
+	//}
+	//
+//}
+
+
+//////////////////////////////////////////////////////////////////////////
+//						KOD FÖR SPI										//
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+unsigned char fromSen[16];
+unsigned char fromKom[16];
+uint8_t SPIcounter;
+uint8_t toSen;
+uint8_t toKom;
+uint8_t overflow;
+// timer0 interruppt funktion
+ISR(TIMER0_OVF_vect)
+{
+	++overflow;
+	
+	if(overflow >= 2000){
+		SPI_sen_transmit_master();
+	overflow = 0;
+	}
+}
+void SPI_sen_transmit_master()
+{
+	SPIcounter = 0;
+	//Sätter SS för styr->sen
+	toSen = 0;
+	toKom = 1;
+	Set_SS_sen_kom(toSen, toKom);
+	// skickar noll till Sen
+	SPDR = fromSen[0];
+}
+ISR(SPI_STC_vect)
+{
+	fromSen[0] = 14;
+	fromSen[1] = 15;
+	fromSen[2] = 36;
+	fromSen[3] = 47;
+
+	/*
+	if(toSen == 1){
+		fromSen[SPIcounter] = SPDR;
 		
+		if(SPIcounter < 15){
+			++SPIcounter;
+			SPDR = 0;
+		}
+		else{
+			SPIcounter = 0;
+			//sätter SS till styr->kom
+			toSen = 0;
+			toKom = 1;
+			Set_SS_sen_kom(toSen, toKom);
+			//Startar styr->kom buss
+			SPDR = fromSen[SPIcounter];
+		}
+	}
+	else if(toKom == 1){
+		fromKom[SPIcounter] = SPDR;
 		
+		if(SPIcounter < 15){
+			SPDR = fromSen[SPIcounter + 1];
+			++SPIcounter;
+		}
+		else{
+			SPIcounter = 0;
+			toSen = 0;
+			toKom = 0;
+			Set_SS_sen_kom(toSen, toKom);
+		}
+	}
+	*/
+	if(toKom == 1){
+		fromKom[SPIcounter] = SPDR;
+		
+		if(SPIcounter < 15){
+			_delay_ms(1);
+			SPDR = fromSen[SPIcounter + 1];
+			++SPIcounter;
+		}
+		else{
+			SPIcounter = 0;
+			toSen = 0;
+			toKom = 0;
+			Set_SS_sen_kom(toSen, toKom);
+		}
+	}
+}
+void Set_SS_sen_kom(uint8_t toSen, uint8_t toKom)
+{
+	if(toSen == 1){
+		PORTB &= ~(1 << SS_sen);
+		PORTB |= (1 << SS_kom);
+	}
+	else if(toKom == 1){
+		PORTB |= (1 << SS_sen);
+		PORTB &= ~(1 << SS_kom);
+	}
+	else{
+		PORTB |= (1 << SS_sen);
+		PORTB |= (1 << SS_kom);
+	}
+}
+void SPI_init_master()
+{
+	//MOSI, SCK , SS0 och SS1 är utgångar
+	DDRB |= 0b10111000;	
+	overflow = 0;
+	//SPI Enable
+	//SPI Master
+	//SPI clock f/16
+	//SPI interrupt enable
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPIE);
+	
+	// Enable timer0 interruppt
+	TIMSK0 = (1<<TOIE0);
+	// set timer0 counter initial value to 0
+	TCNT0=0x00;
+	// start timer0 with /1024 prescaler
+	TCCR0B = (1<<CS02) | (1<<CS00);
+	
+	SPIcounter = 0;
+	uint8_t toSen = 0;
+	uint8_t toKom = 0;
+	
+	Set_SS_sen_kom(toSen, toKom);
+}
