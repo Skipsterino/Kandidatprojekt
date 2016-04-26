8413233ef81d370a9ada2a1223ebc644e63545b8
green-pirate <ehn.jonas@gmail.com>
2016-04-26 15:49:21 +0200

    menade sensor.h

--- sensor/sensor/sensor.h
+++ sensor/sensor/sensor.h
@@ -1,10 +1,12 @@
-/*
- * sensor.h
- *
- * Created: 4/12/2016 8:59:49 AM
- *  Author: joneh084
- */ 
-
+/**
+* @file sensor.h
+* @author Fredrik, Jonas
+* @date 26 apr 2016
+* @brief The code governing the behaviour of the sensor module
+*
+* The code that governs the sensor module. Handles the collection of all the sensor 
+* data. 
+*/
 
 #ifndef SENSOR_H_
 #define SENSOR_H_
@@ -18,74 +20,75 @@
 #include <util/delay.h>
 #include <compat/twi.h>
 
-#define x 0
-#define y 1
-#define z 2
+#define x 0				/**< Define the x as zero for index in matrices */
+#define y 1				/**< Define the y as one for index in matrices */
+#define z 2				/**< Define the z as two for index in matrices */
 
-volatile uint8_t SPI_done = 0;
-volatile uint8_t SPI_overflow = 0;
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const int delay_time = 50;					// tid i millisekunder (50 ger alltså ungefär 20Hz, lagom för US-sensorn)
-
-const double IR_sensor_distance_right = 16.5;		// Avståndet mellan högra sidosensorerna (cm)
-const double IR_sensor_distance_left = 16.5;		// Avståndet mellan vänstra sidosensorerna (cm)
+volatile uint8_t SPI_done = 0;		/**< Flag for if there's been a SPI transmission recently */
+volatile uint8_t SPI_overflow = 0;	/**< Help variable for the timer since last SPI transmission */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-volatile double IR_latest_reading[7];
-double IR_reading[7][5];							// 2D-array med de 5 senaste avläsningarna för de 7 sensorerna
-double IR_ADC[7], IR_distance[7];
-float IR_Yaw_right, IR_Yaw_left;					
+const int delay_time = 50;			/**< A delay for testing to simulate activity on the SPI-bus */
 
+const double IR_sensor_distance_right = 16.5;		/**< Distance between the two IR-sensors on the right side [cm] */
+const double IR_sensor_distance_left = 16.5;		/**< Distance between the two IR-sensors on the left side [cm] */
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+volatile double IR_latest_reading[7];	/**< The most recent readings from all the IR-sensors */
+double IR_reading[7][5];				/**< 2D-array with the 5 latest readings for the 7 IR-sensors */
+double IR_ADC[7];						/**< Average value for the last 5 readings for the 7 IR-sensors */
+double IR_distance[7];					/**< Distance measured by the 7 IR-sensors */
+float IR_Yaw_right, IR_Yaw_left;		/**< Yaw angle calculated from the IR-measurements on each side */			
+
+/**
+* Struct that contains the tables that describe the conversion from sensor readings to distances
+*/
 typedef struct
 {
 	double ADC_data;
 	double distance;
 } ADC_distance_pair;
 
-volatile double US_reading;									// (US = Ultra Sound)
-double US_distance;
+volatile double US_reading;				/**< The most recent reading from the US sensor */
+double US_distance;						/**< Distance calculated from the US sensor */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-uint8_t buffer0_IR0 = 0xf0;
-uint8_t buffer1_IR1 = 0xf1;
-uint8_t buffer2_IR2 = 0xf2;
-uint8_t buffer3_IR3 = 0xf3;
-uint8_t buffer4_IR4 = 0xf4;
-uint8_t buffer5_IR5 = 0xf5;
-uint8_t buffer6_IR6 = 0xf6;
-uint8_t buffer7_US = 0xf7;		// Unsigned 8-bitars int, 0 - 255
-int8_t buffer8_IR_Yaw_left = 0xf8;
-int8_t buffer9_IR_Yaw_right = 0xf9;
-int8_t buffer10_IMU_Yaw_Low = 0xfa;
-int8_t buffer11_IMU_Yaw_High = 0xfb;
-int8_t buffer12_Pitch = 0xfc;
-int8_t buffer13_Roll =0xfd;		// Signed 8-bitars int
+uint8_t buffer0_IR0 = 0xf0;				/**< Buffer containing distance from IR_0 to be put on the bus */
+uint8_t buffer1_IR1 = 0xf1;				/**< Buffer containing distance from IR_1 to be put on the bus */
+uint8_t buffer2_IR2 = 0xf2;				/**< Buffer containing distance from IR_2 to be put on the bus */
+uint8_t buffer3_IR3 = 0xf3;				/**< Buffer containing distance from IR_3 to be put on the bus */
+uint8_t buffer4_IR4 = 0xf4;				/**< Buffer containing distance from IR_4 to be put on the bus */
+uint8_t buffer5_IR5 = 0xf5;				/**< Buffer containing distance from IR_5 to be put on the bus */
+uint8_t buffer6_IR6 = 0xf6;				/**< Buffer containing distance from IR_6 to be put on the bus */
+uint8_t buffer7_US = 0xf7;				/**< Buffer containing distance from US to be put on the bus */
+int8_t buffer8_IR_Yaw_left = 0xf8;		/**< Buffer containing Yaw angle calculated with IR sensors on the left side to be put on the bus */
+int8_t buffer9_IR_Yaw_right = 0xf9;		/**< Buffer containing Yaw angle calculated with IR sensors on the right side to be put on the bus */
+int8_t buffer10_IMU_Yaw_Low = 0xfa;		/**< Buffer containing low bits of the Yaw angle from the IMU */
+int8_t buffer11_IMU_Yaw_High = 0xfb;	/**< Buffer containing high bits of the Yaw angle from the IMU */
+int8_t buffer12_Pitch = 0xfc;			/**< Buffer containing Pitch angle from the IMU */
+int8_t buffer13_Roll = 0xfd;			/**< Buffer containing Roll angle from the IMU */
 
-volatile int byte_to_send = 0;			// Vilken byte i bufferten som skall skickas härnäst
+volatile int byte_to_send = 0;			/**< Counter on which byte to send next */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define QUAT_W 0
-#define QUAT_X 1
-#define QUAT_Y 2
-#define QUAT_Z 3
+#define QUAT_W 0	/**< Define index 0 as quaternion w */
+#define QUAT_X 1	/**< Define index 1 as quaternion x */
+#define QUAT_Y 2	/**< Define index 2 as quaternion y */
+#define QUAT_Z 3	/**< Define index 3 as quaternion z */
 
-#define MPU_HZ 12		//frekvensen som IMU:n genererar avbrott med (internt kör DMP:n alltid med 200 Hz)
-#define USE_DMP 1
+#define MPU_HZ 12	/**< Frequency with which the IMU generates interrupts (DMP always running at 200 Hz internally) */
+#define USE_DMP 1	/**< Use the internal DMP */
 
-volatile unsigned char IMU_data_ready = 0;
+volatile unsigned char IMU_data_ready = 0;							/**< Flag stating if there's been a interrupt by the IMU */
 
-const signed char _orientation[9] = {1, 0, 0,  0, 1, 0,  0, 0, 1};
+const signed char _orientation[9] = {1, 0, 0,  0, 1, 0,  0, 0, 1};	/**< Rotation matrix describing the mounting of the IMU */
 
-float quaternion[4];
-int16_t temp_reading;
-double IMU_temperature;
-float gravity[3];
-float IMU_Yaw, IMU_Pitch, IMU_Roll;
+float quaternion[4];					/**< Quaternions describing the orientation of the IMU */				
+float gravity[3];						/**< The gravity vectors used to remove the effect of the gravity on the measurements from the IMU */
+float IMU_Yaw, IMU_Pitch, IMU_Roll;		/**< The orientation of the IMU in Euler angles */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -117,7 +120,7 @@ ADC_distance_pair IR0_table[] =
 	{501.9, 25},
 	{554.3, 20},
 	{572.5, 15}
-};
+}; /**< Table describing relation between sensor value to distance for IR sensor 0 */
 ADC_distance_pair IR1_table[] =
 {
 	{39.3, 150},
@@ -133,7 +136,7 @@ ADC_distance_pair IR1_table[] =
 	{284.3, 20},
 	{383.8, 15},
 	{576.1, 10}
-};
+};	/**< Table describing relation between sensor value to distance for IR sensor 0 */
 ADC_distance_pair IR2_table[] =
 {
 	{42.4, 240},
@@ -155,7 +158,7 @@ ADC_distance_pair IR2_table[] =
 	{483.5, 25},
 	{531.0, 20},
 	{561.3, 15}
-};
+};	/**< Table describing relation between sensor value to distance for IR sensor 0 */
 ADC_distance_pair IR3_table[] =
 {
 	{45.3, 240},
@@ -177,7 +180,7 @@ ADC_distance_pair IR3_table[] =
 	{499.4, 25},
 	{550.7, 20},
 	{574.7, 15}
-};
+};	/**< Table describing relation between sensor value to distance for IR sensor 0 */
 ADC_distance_pair IR4_table[] =
 {
 	{36.6, 150},
@@ -193,7 +196,7 @@ ADC_distance_pair IR4_table[] =
 	{285.7, 20},
 	{390.1, 15},
 	{570.8, 10},
-};
+};	/**< Table describing relation between sensor value to distance for IR sensor 0 */
 ADC_distance_pair IR5_table[] =
 {
 	{47.7, 240},
@@ -215,7 +218,7 @@ ADC_distance_pair IR5_table[] =
 	{496.8, 25},
 	{548.2, 20},
 	{562.6, 15}
-};
+};	/**< Table describing relation between sensor value to distance for IR sensor 0 */
 ADC_distance_pair IR6_table[] =
 {
 	{43.0, 240},
@@ -237,33 +240,135 @@ ADC_distance_pair IR6_table[] =
 	{490.3, 25},
 	{544.7, 20},
 	{576.6, 15}
-};
+};	/**< Table describing relation between sensor value to distance for IR sensor 0 */
 
 ///////////////////////////////////////////////////////////////////////////////////////
 ///									FUNKTIONER										///
 ///////////////////////////////////////////////////////////////////////////////////////
-
-//initiering
+/**
+* @brief Initiates the A/D-converter
+*
+* Initiates the A/D-converter and enables noice cancelling mode.
+*/
 void init_ADC();
+
+/**
+* @brief Initiates the ultra sound sensor
+*
+* Initiates the ultra sound sensor and allows interrupts from it.
+*/
 void init_US();
+
+/**
+* @brief Initiates the SPI-bus
+*
+* Initiates the SPI-bus and start a timer to keep the bus in sync.
+*/
 void init_SPI();
+
+/**
+* @brief Initiates a timer 
+*
+* Initiates a timer to give us a timestamp.
+*/
 void init_timer();
+
+/**
+* @brief Initiates the I2C-bus
+*
+* Initiates the I2C-bus which enables communication with the IMU.
+*/
 void init_I2C();
+
+/**
+* @brief Initiates the IMU
+*
+* Initiates the IMU by waking it up from sleep mode and programming the DMP.
+*/
 void init_IMU();
+
+/**
+* @brief Runs a self-test on the IMU
+*
+* Runs a self-test on the IMU calibrating the gyros and accelerometers. 
+*/
 void run_self_test();
+
+/**
+* @brief Initiates the interrupt from the IMU
+*
+* Enables a interrupt from the IMU. Interrupt comes when data is available.
+*/
 void init_IMU_interrupt();
 
+/**
+* @brief Samples data from the IR sensors
+*
+* Samples from all the IR-sensors, saves them in a buffer with the five latest measurements for each sensor and takes an average.
+*/
 void ADC_IR();
+
+/**
+* @brief Gathers data from the IMU
+*
+* Gathers data from the IMU and calculates roll, yaw and pitch angle from returned quaternions.
+*/
 void read_IMU();
+
+/**
+* @brief Forces a sound pulse from the US
+*
+* Sends a trigger pulse for 12µs. Starts a timer that waits for a answer from the US-sensor.
+*/
 void send_ping();
+
+/**
+* @brief Converts the readings from the IR sensors to distances
+*
+* Converts the IR-sensors values from the A/D-converter trough linear interpolation and the tables.
+*/
 void ADC_to_distance();
+
+/**
+* @brief Looks up equivalent distance to a ADC value in the table
+*
+* Uses the table to convert the values from the ADC to distances using the tables and linear interpolation.
+*/
 double lookup_distance(ADC_distance_pair* ADC_dist_table, double ADC_value, int table_size);
+
+/**
+* @brief Converts time for the US to distance
+*
+* Calculates a distance for the US sensor using the time-of-flight for the sound.
+*/
 void time_to_distance();
+
+/**
+* @brief Calculate Yaw angle from the values from the IR sensors
+*
+* Uses the distances from the IR sensors to calculate a Yaw angle relative to the corridor using trigonometry.
+*/
 void calculate_Yaw();
+
+/**
+* @brief Saves all the data in a buffer
+*
+* Converts and compiles all the data in a buffer, ready to be sent.
+*/
 void save_to_buffer();
 
-//Hjälpfunktioner
+/**
+* @brief Normalizes a quaternion
+*
+* Normalizes a quaternion
+*/
 void NormalizeQuaternion(float *quat);
+
+/**
+* @brief Restricts an angle to range for int_8t
+*
+* Restricts an angle to be between -128 and 127 deg to avoid overflow when converting to int_8t
+*/
 float restrict_angle(float angle);
 
 void kalibrering();		// XXXXX Endast för att kunna kalibrera sensorer!
