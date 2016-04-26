52708ac3f2bf80221827d8a949ce74ee1020d73b
Skipsterino <kevin.kjellen@live.se>
2016-04-26 13:55:51 +0200

    Fixade kommentering i SPI.h för styr

--- styr/styr/SPI.h
+++ styr/styr/SPI.h
@@ -1,32 +1,53 @@
-﻿/*
- * SPI.h
- *
- * Created: 4/8/2016 2:53:17 PM
- *  Author: chrma018
- */ 
-
+/**
+* @file SPI.h
+* @author Christoffer
+* @date 26 apr 2016
+* @brief SPI Driver
+*
+* Defines communication through the SPI bus given a triggered interruppt
+*/
 
 #ifndef SPI_H_
 #define SPI_H_
 
 #ifndef F_CPU
-#define F_CPU 16000000UL		// 16 MHz
+#define F_CPU 16000000UL //*< Speed of processor (16 Mhz) */
 #endif
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define SS_sen 3
-#define SS_kom 4
+#define SS_sen 3 //*< Offset of the slave-select bit to Sensor module */
+#define SS_kom 4 //*< Offset of the slave-select bit to Kommunacation module */
 
-volatile unsigned char fromSen[16];
-volatile unsigned char fromKom[16];
+volatile unsigned char fromSen[16]; //*< Array in which data from Sensor module is stored */
+volatile unsigned char fromKom[16]; //*< Array in which data form Kommunication module is stored */
 
-volatile unsigned char lastValidPacket[16];
+volatile unsigned char lastValidPacket[16]; //*< Array in which the last checksum-valid fromKom array is stored */
 
+/**
+* @brief Initiation
+*
+* Initiates the outputs and SPI configuration as
+* a master on the bus
+*/
 void SPI_init_master();
+
+/**
+* @brief Master transmit
+*
+* Transmits the first byte to Sensor Module
+* starting the chain of 16
+*/
 void SPI_sen_transmit_master();
+
+/**
+* @brief Change slave select
+*
+* Sets the slave select ports to their respective
+* value given 'bools' of what they should be
+*/
 void Set_SS_sen_kom(uint8_t toSen, uint8_t toKom);
 
 #endif /* SPI_H_ */
